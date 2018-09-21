b4b3f11b978e08623bb965ce33e700a3dec4047d
nonate-tw <nonate@thoughtworks.com>
2018-09-20 21:39:24 -0300

    init

---
+++ src/main/java/com/example/myanimelist/MyanimelistApplication.java
@@ -0,0 +1,12 @@
+package com.example.myanimelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyanimelistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyanimelistApplication.class, args);
+	}
+}
